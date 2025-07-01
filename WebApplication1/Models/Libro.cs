96d7cf9ffe9f158afd2a0d7fa7a7cb046beb8c66
Alex Espinoza <alexespinozacangas2018@gmail.com>
2025-07-01 12:40:28 -0500

    Agregar clase Libro modelo

---
+++ WebApplication1/Models/Libro.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class Libro
+
+    {
+
+        public string? titulo { get; set; }
+        public string? autor { get; set; }
+
+        public string? genero { get; set; }
+        public string? isbn { get; set; }
+        public int? anioPublicacion { get; set; }
+        public string? editorial { get; set; }
+        public string? sinopsis { get; set; }
+         
+
+    }
+}
---
+++ WebApplication1/Models/Sucursal.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Sucursal
+    {
+        public int sucursalId { get; set; }
+
+        public string? nombre { get; set; }
+        public string? direccion { get; set; }
+        public string? telefono { get; set; }
+        public string? email { get; set; }
+        
+
+    }
+}
